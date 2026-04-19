07ac9ff200fb24901a5b2b6ca55822d99f7439dd
antoinerougier <antoinerougier77@gmail.com>
2026-04-19 20:04:32 +0200

    Feat: add triple

--- app.py
+++ app.py
@@ -12,5 +12,14 @@ def square():
         return jsonify({"error": "Paramètre 'n' invalide ou manquant"}), 400
 
 
+@app.route("/triple", methods=["GET"])
+def square():
+    try:
+        n = float(request.args.get("n"))
+        return jsonify({"input": n, "result": n**3})
+    except (TypeError, ValueError):
+        return jsonify({"error": "Paramètre 'n' invalide ou manquant"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
